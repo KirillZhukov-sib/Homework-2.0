639754a48714d23dfe0ee787c594b8811c4e2a6a
Кирилл Жуков <kirillsiber@yandex.ru>
2025-06-26 00:37:49 +0700

    8

---
+++ Task8_1/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task8_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Напишите первое число");
+                double x = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Напишите второе число");
+                double y = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Напишите какую операцию хотите выполнить с цифрами:");
+                Console.WriteLine("1 - сложение");
+                Console.WriteLine("2 - вычитание");
+                Console.WriteLine("3 - умножение");
+                Console.WriteLine("4 - деление");
+                int z = Convert.ToInt32(Console.ReadLine());
+
+                double result = Count(x, y, z);
+                Console.WriteLine("Ответ: {0:0.00}", result);
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Попытка деления на ноль");
+            }
+            
+            catch (FormatException)
+            {
+                Console.WriteLine("Введено не число");
+            }
+        }
+
+        static double Count(double x, double y, int z)
+        {
+            switch (z)
+            {
+                case 1: return x + y;
+                case 2: return x - y;
+                case 3: return x * y;
+                case 4:
+                    if (y == 0) throw new DivideByZeroException();
+                    return x / y;
+                default: throw new ArgumentException("Неверный код операции");
+
+            }
+        }
+    }
+}
---
+++ Task8_2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task8_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Укажите свой возраст");
+                int age = Convert.ToInt32(Console.ReadLine());
+                Validate(age);
+                Console.WriteLine("Ваш возраст принят");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Введено не число");
+            }
+        }
+
+        static void Validate(int age)
+        {
+            if (age < 0)
+                throw new ArgumentException("Возраст не может быть отрицательным");
+            if (age > 100)
+                throw new ArgumentOutOfRangeException("Слишком большой возраст");
+        }
+
+    }
+
+}
