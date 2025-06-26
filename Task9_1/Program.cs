5dccfa907083c8eaa8c122cca3f2c0ab0cc116f0
Кирилл Жуков <kirillsiber@yandex.ru>
2025-06-26 19:57:58 +0700

    9

--- Task9_1/Program.cs
+++ Task9_1/Program.cs
@@ -10,6 +10,41 @@ namespace Task9_1
     {
         static void Main(string[] args)
         {
+            //Cat cat = new Cat("Мурка", "черный", 1);
+            //Cat cat2 = new Cat("Мурка", "черный", 1);
+
+            Book book1 = new Book("War and peace", "L. Tolstoy", "1869", "1225 pages");
+
+            book1.year = "2025";
+            book1._author = "Pushkin";
+            //book1._title = "Capitans dauther";
+            book1.pages = "231";
+
+            book1.GetInfo();
+
+            Console.ReadKey();
+        }
+    }
+
+    public class Book
+    {
+        public string _title;
+        public string _author;
+
+        public string year { get; set; }
+        public string pages { get; set; }
+
+        public Book(string _title, string _author, string year, string pages)
+        {
+            this._title = _title;
+            this._author = _author;
+            this.year = year;
+            this.pages = pages;
+        }
+
+        public void GetInfo()
+        {
+            Console.WriteLine($"{_title}, {_author}, {year}, {pages}");
         }
     }
 }
---
+++ Task9_2/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task9_2
+{
+    public class BankAccount
+    {
+        private static int _totalAccounts = 0;
+        private static readonly Random _rndacc = new Random();
+        public int AccountNumber { get; }
+        public decimal Balance { get; private set; }
+
+        public BankAccount()
+        {
+            _totalAccounts++;
+            AccountNumber = _rndacc.Next(0, 10000);
+            Balance = 0;
+        }
+
+        public void Deposit(decimal amount)
+        {
+            Balance += amount;
+        }
+
+        public void Withdraw(decimal amount)
+        {
+            if (amount <= 0)
+                throw new ArgumentException("Сумма должна быть положительной");
+
+            if (amount > Balance)
+                throw new InvalidOperationException("Недостаточно средств на счете");
+            Balance -= amount;
+        }
+
+        public static void ShowTotalAccounts()
+        {
+            Console.WriteLine("Всего счетов в банке {0}", _totalAccounts);
+        }
+        public override string ToString()
+        {
+            return string.Format("Счет: {0}, Баланс {1} руб.", AccountNumber, Balance);
+        }
+    }
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount account1 = new BankAccount();
+            Console.WriteLine(account1.ToString());
+            account1.Deposit(100);
+            Console.WriteLine($"Баланс после пополнения: {account1.Balance} руб.");
+            account1.Withdraw(50);
+            Console.WriteLine($"Баланс после снятия: {account1.Balance} руб.");
+
+            BankAccount account2 = new BankAccount();
+            Console.WriteLine(account2.ToString());
+            account2.Deposit(250);
+            Console.WriteLine($"Баланс после пополнения: {account2.Balance} руб.");
+            account2.Withdraw(30);
+            Console.WriteLine($"Balance after withdraw: {account2.Balance} руб.");
+
+            BankAccount.ShowTotalAccounts();
+        }
+    }
+}
