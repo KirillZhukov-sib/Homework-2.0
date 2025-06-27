ab757da5513aa31ab29880a9c99a3ac536807546
Кирилл Жуков <kirillsiber@yandex.ru>
2025-06-27 11:43:14 +0700

    10

--- Task10_1/Program.cs
+++ Task10_1/Program.cs
@@ -10,6 +10,60 @@ namespace Task10_1
     {
         static void Main(string[] args)
         {
+            Triangle triangle = new Triangle();
+            RectTriangle triangleRect = new RectTriangle();
+            Object.
         }
     }
+
+
+    class Triangle
+    {
+        public double A { get; set; }
+        public double B { get; set; }
+        public double C { get; set; }
+
+        public double GetArea()
+        {
+            double p = (A + B + C) / 2;
+            return Math.Sqrt(p * (p - A) * (p - A) * (p - A));
+        }
+    }
+
+
+    class RectTriangle:Triangle
+    {
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    class Example
+    {
+        public AppDomain Domain { get; set; }
+
+
+
+
+
+
+
+
+
+
+
+
 }
