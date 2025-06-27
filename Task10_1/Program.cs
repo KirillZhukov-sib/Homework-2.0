3ec3b74ff6c9308a7f7ac5478b279f24e2388f62
Кирилл Жуков <kirillsiber@yandex.ru>
2025-06-27 15:47:40 +0700

    10

--- Task10_1/Program.cs
+++ Task10_1/Program.cs
@@ -10,61 +10,45 @@ namespace Task10_1
     {
         static void Main(string[] args)
         {
-            Triangle triangle = new Triangle();
-            RectTriangle triangleRect = new RectTriangle();
-
+           
         }
     }
 
 
-    class Triangle
-    {
-        public double A { get; set; }
-        public double B { get; set; }
-        public double C { get; set; }
+    //class Triangle
+    //{
+    //    protected double a, b , c;
+       
 
-        public double GetArea()
-        {
-            double p = (A + B + C) / 2;
-            return Math.Sqrt(p * (p - A) * (p - A) * (p - A));
-        }
-    }
+    //    public Triangle(double a, double b, double c)
+    //    {
+    //        this.a = a;
+    //        this.b = b;
+    //        this.c = c;
+    //    }
+    //    public double GetArea()
+    //    {
+    //        double p = (a + b + c) / 2;
+    //        return Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+    //    }
+    //}
 
+    //class RectTriangle : Triangle
+    //{
+    //    public RectTriangle(double a, double b)
+    //: base(a, b, Math.Sqrt(a * a + b * b))
+    //    {
 
-    class RectTriangle : Triangle
-    {
-
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    public class Building
-    {
-        public string _adress;
-        public double _area;
-        public int _yearbuilt;
-
-        
-
-        public Building(string adress, double area, int yearbuilt)
-        {
-            this._adress = adress;
-            this._area = area;
-            this._yearbuilt = yearbuilt;
-        }
+    //    }
+    //    public bool IsPythagorean
+    //    {
+    //        get
+    //        {
+    //            return A % 1 == 0 && B % 1 == 0 && C % 1 == 0;
+    //        }
+    //    }
+    //}
+     
 
 
 
@@ -78,7 +62,36 @@ namespace Task10_1
 
 
 
+    //public class Building
+    //{
+    //    public string _adress;
+    //    public double _area;
+    //    public int _yearbuilt;
+
+    //    private int buildingAge { get; set; readonly}
+
+    //    public Building(string adress, double area, int yearbuilt)
+    //    {
+    //        this._adress = adress;
+    //        this._area = area;
+    //        this._yearbuilt = yearbuilt;
+    //    }
 
 
-    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
+
