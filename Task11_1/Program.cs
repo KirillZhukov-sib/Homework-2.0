6c3760ca061e47dac524b793c1f7cd7d2811c41d
Кирилл Жуков <kirillsiber@yandex.ru>
2025-06-30 23:27:45 +0700

    11

---
+++ Task11_1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task11_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+        }
+
+
+
+
+    }
+    public abstract class Geometricshapes
+    {
+        public abstract string Dame
+        { get; }
+
+        public void Say()
+        {
+
+
+        }
+
+
+    }
+}
---
+++ Task11_2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task11_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+        }
+
+    }
+}
