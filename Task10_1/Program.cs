5de9d3632c4d5db18fa58572faabbc0fecc76573
Кирилл Жуков <kirillsiber@yandex.ru>
2025-06-30 14:34:25 +0700

    10

--- Task10_1/Program.cs
+++ Task10_1/Program.cs
@@ -10,74 +10,38 @@ namespace Task10_1
     {
         static void Main(string[] args)
         {
-           
+
         }
     }
 
 
-    //class Triangle
-    //{
-    //    protected double a, b , c;
-       
+    public class Building
+    {
+        private string _adress;
+        private double _area;
+        private int _yearbuilt;
 
-    //    public Triangle(double a, double b, double c)
-    //    {
-    //        this.a = a;
-    //        this.b = b;
-    //        this.c = c;
-    //    }
-    //    public double GetArea()
-    //    {
-    //        double p = (a + b + c) / 2;
-    //        return Math.Sqrt(p * (p - a) * (p - b) * (p - c));
-    //    }
-    //}
+        public string BuildingAge(get;)
 
-    //class RectTriangle : Triangle
-    //{
-    //    public RectTriangle(double a, double b)
-    //: base(a, b, Math.Sqrt(a * a + b * b))
-    //    {
+        public Building(string adress, double area, int yearbuilt)
+        {
+            this._adress = adress;
+            this._area = area;
+            this._yearbuilt = yearbuilt;
+        }
 
-    //    }
-    //    public bool IsPythagorean
-    //    {
-    //        get
-    //        {
-    //            return A % 1 == 0 && B % 1 == 0 && C % 1 == 0;
-    //        }
-    //    }
-    //}
-     
+        public virtual void Calculatetax(double tax )
+        { tax = _area * 1000; }
+
+        public virtual void Displayinfo(double tax)
+        { Console.WriteLine(_adress, _area, _yearbuilt); }
 
 
 
 
-
-
-
-
-
-
-
-
-
-    //public class Building
-    //{
-    //    public string _adress;
-    //    public double _area;
-    //    public int _yearbuilt;
-
-    //    private int buildingAge { get; set; readonly}
-
-    //    public Building(string adress, double area, int yearbuilt)
-    //    {
-    //        this._adress = adress;
-    //        this._area = area;
-    //        this._yearbuilt = yearbuilt;
-    //    }
-
-
+    }
+}
+}
 
 
 
