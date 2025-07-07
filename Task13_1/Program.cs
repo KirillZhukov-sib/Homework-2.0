364939050550993d6145b11a743fa58d2399584f
Кирилл Жуков <kirillsiber@yandex.ru>
2025-07-07 12:11:31 +0700

    13

---
+++ Task13_1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task13_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] example = { 1, 2, -3, 4, -5 };
+
+            //удвоение чисел 
+            int Double(int x)
+            { return x * 2; }
+            var result1 = Transform(example, Double);
+            Console.WriteLine($"Результат удвоения чисел: {string.Join(", ", result1)}");
+
+            //Возведение в квадрат
+            int Pow(int x) { return x * x; }
+            var result2 = Transform(example, Pow);
+            Console.WriteLine($"Результат возведения в квадрат: {string.Join(", ", result2)}");
+
+            //Замена числа на их модули 
+            int Module(int x ) 
+            {  return  Math.Abs(x); }
+            var result3 = Transform(example, Module);
+            Console.WriteLine($"Результат приведения к модулю: {string.Join(", ", result3)}");
+
+        }
+
+        public delegate int Transformer(int a);
+        public static int[] Transform(int[] numbers, Transformer transformer)
+        {
+            int[] result = new int[numbers.Length];
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                result[i] = transformer(numbers[i]);
+            }
+            return result;
+        }
+    }
+}
---
+++ Task13_2/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task13_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var home = new SmartHomeSystem();
+            home.DeviceChanged += (device, status) =>
+            {
+                Console.WriteLine($"[{DateTime.Now:HH:mm:ss}] {device}: {status}");
+            };
+            home.UnLockDoor();
+            home.TurnOnLight();
+            home.SetTemperature(20);
+        }
+    }
+
+    public delegate void DeviceStateChanged(string device, string state);
+    public class SmartHomeSystem
+    {
+        public event DeviceStateChanged DeviceChanged;
+        bool Light { get; set; }
+        int Thermostat { get; set; }
+        bool Door { get; set; }
+
+        public SmartHomeSystem()
+        {
+            Light = false;
+            Thermostat = 20;
+            Door = false;
+        }
+
+        public void TurnOnLight()
+        {
+            Light = true;
+            DeviceChanged?.Invoke("Свет", "Включен");
+        }
+        public void TurnOffLight()
+        {
+            Light = false;
+            DeviceChanged?.Invoke("Свет", "Выключен");
+        }
+        public void SetTemperature(int temperature)
+        {
+            Thermostat = temperature;
+            DeviceChanged?.Invoke("Термостат", $"Температура изменена на {temperature}C");
+        } 
+        public void UnLockDoor()
+        {
+            Door = true;
+            DeviceChanged?.Invoke("Дверь", "Открыта");
+        }
+        public void LockDoor()
+        {
+            Door = false;
+            DeviceChanged?.Invoke("Дверь", "Закрыта");
+        }
+    }
+}
