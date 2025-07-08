e89f8b653b6844804a336269aa867368c0366f24
Кирилл Жуков <kirillsiber@yandex.ru>
2025-07-08 09:08:23 +0700

    14

---
+++ Task14_1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task14_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> tasks = new List<string>();
+
+            tasks.Add("Помыть посуду");
+            tasks.Add("Купить продукты");
+            tasks.Add("Покорпить кота");
+
+            tasks.Insert(1, "Сделать зарятку");
+
+            foreach (string task in tasks)
+            {
+                Console.WriteLine(task);
+            }
+
+            bool cats = tasks.Contains("Покормить кота");
+            Console.WriteLine($"Если задача {cats}");
+
+
+
+        }
+
+
+    }
+
+
+}
