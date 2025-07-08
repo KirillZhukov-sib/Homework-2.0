475bf5b612d187a23ceee93dac21b1f17e0afe71
Кирилл Жуков <kirillsiber@yandex.ru>
2025-07-08 14:23:16 +0700

    15

---
+++ Task15_1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task15_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 4;
+            int name = a.GetHashCode();
+            Console.WriteLine(name);
+
+        }
+    }
+}
